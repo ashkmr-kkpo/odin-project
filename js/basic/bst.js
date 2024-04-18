15260ff2aca27ed0cb8ff4bdb0bb601f9c445e91
Ashish Kumar Sathish <ashishcl2096@gmail.com>
2024-04-17 23:50:59 -0400

    balance but avoided callback for array push

--- js/basic/bst.js
+++ js/basic/bst.js
@@ -122,13 +122,73 @@ class Tree {
     }
   }
 
-  inorder(root) {
+  inorder(root, callback = () => {}) {
     if (root == null) {
       return;
     }
-    this.inorder(root.left);
-    console.log(root.data);
-    this.inorder(root.right);
+    this.inorder(root.left, callback);
+    callback(root.data);
+    this.inorder(root.right, callback);
+  }
+
+  inorderPush(root, array) {
+    if (root == null) {
+      return;
+    }
+    this.inorderPush(root.left, array);
+    array.push(root.data);
+    this.inorderPush(root.right, array);
+  }
+
+  levelOrder(callback = () => {}) {
+    let visitingStack = [this.root];
+    while (visitingStack.length > 0) {
+      let curr = visitingStack.shift();
+      callback(curr);
+      if (curr.left != null) {
+        visitingStack.push(curr.left);
+      }
+      if (curr.right != null) {
+        visitingStack.push(curr.right);
+      }
+    }
+  }
+
+  heightOfSubtree(node, height) {
+    if (node.left == null && node.right == null) {
+      return height;
+    }
+    let leftNodeHeight = 0;
+    let rightNodeHeight = 0;
+    if (node.left != null) {
+      leftNodeHeight = this.heightOfSubtree(node.left, height + 1);
+    }
+
+    if (node.right != null) {
+      rightNodeHeight = this.heightOfSubtree(node.right, height + 1);
+    }
+    return Math.max(leftNodeHeight, rightNodeHeight);
+  }
+
+  isBalanced() {
+    let leftHeight = 0;
+    if (this.root.left != null) {
+      leftHeight = this.heightOfSubtree(this.root.left, 1);
+    }
+
+    let rightHeight = 0;
+    if (this.root.right != null) {
+      rightHeight = this.heightOfSubtree(this.root.right, 1);
+    }
+    return Math.abs(leftHeight - rightHeight < 2);
+  }
+
+  rebalance() {
+    let sortedTree = [];
+    this.inorderPush(this.root, sortedTree);
+    console.log(sortedTree);
+    let newRoot = Tree.buildTree(sortedTree);
+    this.root = newRoot;
   }
 }
 
@@ -153,7 +213,7 @@ tree.insert(4);
 tree.insert(8);
 tree.insert(7);
 tree.insert(98);
-tree.inorder(tree.root);
+//tree.inorder(tree.root, console.log);
 console.log(prettyPrint(root));
 tree.deleteItem(7);
 tree.deleteItem(22);
@@ -161,7 +221,17 @@ tree.deleteItem(1);
 console.log(prettyPrint(root));
 tree.deleteItem(92);
 tree.deleteItem(20);
-
+console.log(prettyPrint(tree.root));
+console.log(tree.heightOfSubtree(tree.root, 1));
+console.log(tree.isBalanced());
+tree.insert(13);
+tree.insert(14);
+tree.insert(15);
+tree.insert(16);
+console.log(prettyPrint(tree.root));
+console.log("height:", tree.heightOfSubtree(tree.root, 1));
+console.log("isBalanced:", tree.isBalanced());
+tree.rebalance();
 console.log(prettyPrint(tree.root));
 
 // let emptyTree = new Tree(null);
