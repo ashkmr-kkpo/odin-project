3845f17d196d12b3f0f90f5edfd1f55b24f5e99d
Ashish Kumar Sathish <ashishcl2096@gmail.com>
2024-04-29 22:29:29 -0400

    battleship mid

--- js/async/battleship/battleship.js
+++ js/async/battleship/battleship.js
@@ -9,7 +9,46 @@ class Ship {
 }
 
 class Gameboard {
-  receiveHit() {}
+  #cells = 10;
+
+  Gameboard() {}
+  receiveHit(cell) {
+    let x = cell.getAttribute("x");
+    let y = cell.getAttribute("y");
+    let player = cell.getAttribute("player");
+  }
+  setupGrids() {
+    for (let i = 0; i < 2; i++) {
+      this.setupGrid(i + 1);
+    }
+  }
+
+  setupGrid(index) {
+    let play = document.getElementById("play");
+    let gridNum = document.createElement("div");
+    gridNum.classList.add("grid");
+    gridNum.classList.add("player" + index);
+    for (let i = 0; i < this.#cells; i++) {
+      for (let j = 0; j < this.#cells; j++) {
+        gridNum.appendChild(this.#createNewCell(20, i, j));
+      }
+    }
+    play.appendChild(gridNum);
+  }
+
+  #createNewCell(playerNum, i, j) {
+    let cell = document.createElement("div");
+    cell.style.backgroundColor = "red";
+    cell.setAttribute("x", i);
+    cell.setAttribute("y", j);
+    cell.setAttribute("player", playerNum);
+    cell.classList.add("cell");
+    cell.addEventListener("click", () => this.receiveHit(cell));
+    return cell;
+  }
 }
 
 class Player {}
+
+let board = new Gameboard();
+board.setupGrids();
