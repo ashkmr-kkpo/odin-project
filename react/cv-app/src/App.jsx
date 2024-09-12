3daf9ba4e97b3070e8bcf71a27d33ed8d4ed4f07
Ashish Kumar Sathish <ashishcl2096@gmail.com>
2024-09-11 23:57:47 -0400

    more formatting

--- react/cv-app/src/App.jsx
+++ react/cv-app/src/App.jsx
@@ -5,18 +5,30 @@ import { useState } from 'react';
 function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [university, setUniversity] = useState('');
+  const [location, setLocation] = useState('');
 
     return (
     <>
     <div id = "main">
       <h1>Resume</h1>
         <div id = "body">
+          <div className = "sec">
           <TextField class="inp" label="First Name" variant="outlined" margin="dense" onChange={e => {setFirstName(e.target.value)}}/>
           <TextField class="inp" label="Last Name" variant="outlined" margin="dense" onChange={e => {setLastName(e.target.value)}}/>
+          </div>
+          <div className = "sec">
+          <TextField class="inp" label="University" variant="outlined" margin="dense" onChange={e => {setUniversity(e.target.value)}}/>
+          <TextField class="inp" label="Location" variant="outlined" margin="dense" onChange={e => {setLocation(e.target.value)}}/>
+          </div>
         </div>
     </div>
     <div id = "cv-body">
-      <h1>{firstName + " " + lastName}</h1>
+      <h1 id="cv-title">{firstName + " " + lastName}</h1>
+      <div id="cv-cont">
+        <p>{university}</p>
+        <p>{location}</p>
+      </div>
     </div>
     </>
   )
