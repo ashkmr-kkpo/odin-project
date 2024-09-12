465d97912a2938c73978eaea3d44ffa958e85b39
Ashish Kumar Sathish <ashishcl2096@gmail.com>
2024-09-11 20:40:08 -0400

    start cv

--- react/cv-app/src/App.jsx
+++ react/cv-app/src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
 import TextField from '@mui/material/TextField'
+import { useState } from 'react';
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
     return (
     <>
     <div id = "main">
-        <div id = "title">
-            <h1>Resume</h1>
-            <TextField id="outlined-basic" label="First Name" variant="outlined" />
-            <TextField id="outlined-basic" label="Last Name" variant="outlined" />
-          </div>
-        <div id = "grid-body">
+      <h1>Resume</h1>
+        <div id = "body">
+          <TextField class="inp" label="First Name" variant="outlined" margin="dense" onChange={e => {setFirstName(e.target.value)}}/>
+          <TextField class="inp" label="Last Name" variant="outlined" margin="dense" onChange={e => {setLastName(e.target.value)}}/>
         </div>
+    </div>
+    <div id = "cv-body">
+      <h1>{firstName + " " + lastName}</h1>
     </div>
     </>
   )
